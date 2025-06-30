e9620f70e5e5a65ff5ebebe1c40c96c17cf1cd15
melinap9omu <aniepomu@gmail.com>
2025-06-30 22:09:50 +0545

    User model for frontend

---
+++ frontend/lib/model/user.dart
@@ -0,0 +1,40 @@
+class User {
+
+  final int id;
+  final String firstname;
+  final String lastname;
+  final String phoneNo;
+  final String email;
+
+
+
+
+  User({required this.id,
+    required this.firstname,
+    required this.lastname,
+    required this.phoneNo,
+    required this.email,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json){
+    return User(
+      id: json['id'] as int,
+      firstname: json['firstName'] as String,
+      lastname: json['lastName'] as String,
+      phoneNo: json ['phoneNo'] as String,
+      email: json['email'] as String,
+    );
+  }
+
+  String get fullName=>'$firstname $lastname';
+
+  Map<String, dynamic> toJson{
+    return{
+  'id': id,
+  'firstName': firstName,
+  'lastName': lastName,
+  'phoneNo': phoneNo,
+  'email': email
+  };
+}
+}
